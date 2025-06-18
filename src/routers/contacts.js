b55c9417acc2e1d747057aa8d63e48c15bade4c3
DemonVenom666 <demonvenom9@gmail.com>
2025-06-18 10:55:45 +0200

    create photo in createControllers

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -12,7 +12,7 @@ router.use(authenticate);
 
 router.get('/', ctrlWrapper(getContactsController));
 router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
-router.post('/', validateBody(createContactSchema), ctrlWrapper(createContactController));
+router.post('/', upload.single('photo'), validateBody(createContactSchema), ctrlWrapper(createContactController));
 router.patch('/:contactId', upload.single('photo'), isValidId, validateBody(updateContactSchema), ctrlWrapper(patchContactController));
 router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
